796ca8c9369534af639ddef7b2643d6e62fc3047
Iamtheflash-four <salvatorecorrera04@gmail.com>
2025-12-16 18:42:21 +0100

    errore main

--- src/main/java/service/IssueMain.java
+++ src/main/java/service/IssueMain.java
@@ -5,8 +5,6 @@ import java.net.URI;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class IssueMain 
 {
