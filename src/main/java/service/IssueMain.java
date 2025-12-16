ad2f2b4354ec1cdcc3bebda93d27495858bdee97
Iamtheflash-four <salvatorecorrera04@gmail.com>
2025-12-16 17:13:03 +0100

    Update IssueMain.java

--- src/main/java/service/IssueMain.java
+++ src/main/java/service/IssueMain.java
@@ -5,17 +5,27 @@ import java.net.URI;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class IssueMain 
 {
 	public static void main(String[] args) 
     {
-         SpringApplication.run(IssueMain.class, args);
+        startServer();
     }
+
+	private static void startServer() {
+		// Legge la porta da variabile d'ambiente, default 8080 se non definita
+		int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8082"));
+		
+        ResourceConfig rc = new ResourceConfig().packages("service");
+        
+        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(
+            URI.create("http://0.0.0.0:" + port), rc);
+        
+        System.out.println("Server avviato su http://0.0.0.0:" + port);
+	}
 }
+
 
 
 
